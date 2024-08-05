0c6fc073e3bcadd47894033c50bfc2cf7a976185
hunain3199 <muhammadhunain3199@gmail.com>
2024-08-05 06:55:45 +0500

    donedan

---
+++ contracts/eventOrganization.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract event_organization{
+
+    enum EventCriteria{ Private,Public,Corperate }
+    address owner = msg.sender;
+    struct Event {
+        string organizorName;
+        string eventName;
+        uint ticketPrice;
+        uint maxtickets;
+        EventCriteria eventCriteria;
+        address organizerAddress;
+}
+
+    struct EventModal {
+        Event[] eventArray;
+    }
+
+    mapping(address => EventModal)  eventmap;
+
+    modifier onlyOwner() {
+    require(msg.sender == owner, "Ownable: caller is not the owner");
+    _;
+}
+    
+    function addEvent(string memory organizorName_,string memory eventName_,uint ticketPrice_,uint maxtickets_,EventCriteria eventCriteria_) public payable returns(string memory) 
+    {
+        require(msg.value == 1 ether, "You must send exactly 1 ether to add an event");
+        Event memory event_ = Event(organizorName_,eventName_,ticketPrice_,maxtickets_,eventCriteria_,msg.sender);
+        eventmap[msg.sender].eventArray.push(event_);
+        return "Event Added Successfully";
+    }
+
+    
+    function showEvent(address organizerAddress_) public view onlyOwner returns(EventModal memory) 
+     {
+
+        return eventmap[organizerAddress_];
+     }
+
+    function showEventDetails(address _organizerAddress) public view onlyOwner 
+    returns(string memory,string memory,uint,uint,EventCriteria)
+    {
+        string memory organizorName_ = eventmap[_organizerAddress].eventArray[eventmap[_organizerAddress].eventArray.length - 1].organizorName;
+        string memory eventName_ = eventmap[_organizerAddress].eventArray[eventmap[_organizerAddress].eventArray.length - 1].eventName;
+        uint ticketPrice_ = eventmap[_organizerAddress].eventArray[eventmap[_organizerAddress].eventArray.length - 1].ticketPrice;
+        uint maxtickets_ = eventmap[_organizerAddress].eventArray[eventmap[_organizerAddress].eventArray.length - 1].maxtickets;
+        EventCriteria eventCriteria_ = eventmap[_organizerAddress].eventArray[eventmap[_organizerAddress].eventArray.length - 1].eventCriteria;
+        
+       
+        return ( organizorName_,eventName_,ticketPrice_,maxtickets_,eventCriteria_);
+    }
+
+    function delete_event_data(address _organizerAddress) public  returns(string memory) 
+    {
+        address payable organizor_owner = payable(_organizerAddress);
+        eventmap[_organizerAddress].eventArray.pop();
+        organizor_owner.transfer(1 ether);
+
+        return "Event Deleted Successfully";
+    }
+
+    function getContractBalance() public view onlyOwner returns(uint)
+    {
+        return address(this).balance;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+}
